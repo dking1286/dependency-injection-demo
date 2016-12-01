77e01e3d1d470673228f938c425861a7c2f531b3
Daniel King <daniel.oliver.king@gmail.com>
2016-11-30 19:45:23 -0800

    Remove duplicate test cases

--- dependency_injection.spec.js
+++ dependency_injection.spec.js
@@ -47,31 +47,3 @@ describe('get', function () {
       .to.be.true
   })
 })
-
-describe('onHttpResponse', function () {
-  let resolve;
-  let reject;
-
-  beforeEach(function () {
-    resolve = sinon.spy()
-    reject = sinon.spy()
-  })
-
-  it('should invoke resolve with the response text when the status is 200', function () {
-    const handler = onHttpResponse(resolve, reject)
-    handler({ status: 200, responseText: 'Hi' })
-    
-    expect(resolve.calledWith('Hi')).to.be.true
-    expect(reject.called).to.be.false
-  })
-
-  it('should invoke reject if the status is not 200', function () {
-    const handler = onHttpResponse(resolve, reject)
-    handler({ status: 404, responseText: 'Nope' })
-    
-    expect(reject.calledWith(sinon.match({
-      message: 'Status code was 404, not 200'
-    }))).to.be.true
-    expect(resolve.called).to.be.false
-  })
-})
